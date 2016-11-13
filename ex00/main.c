add01f6fe02176848cb0d8599e015b9e296c0462
Matthew Gould <matt@everettgould.com>
2016-11-13 12:32:30 -0800

    updating main to get input size"

--- ex00/main.c
+++ ex00/main.c
@@ -15,21 +15,58 @@ send those parameters into the rush generators.
 check each rush against the output.
 
 */
+int	input_size(char *buf, int **xy)
+{
+	int i;
+	int j;
+	int line1;
+
+	i = 0;
+	j = 0;
+	while (buf[j])
+	{
+		while((buf[j] != '\n') && buf[j])
+		{
+			j++;
+		}
+		if (i == 0)
+			line1 = j;
+		//if ((j % line1) != 0)
+		//	return 0;
+		i++;
+		j++;
+	}
+	(*xy)[0] = 3;
+	(*xy)[1] = 5;
+	return 0;
+}
 
 int		main(void)
 {
-	char *buf;
+	char	*buf;
+	int		*xy;
 
-	buf = malloc(sizeof(char) * (SIZE + 1));
+	buf = (char*)malloc(sizeof(char) * (SIZE + 1));
+	xy = (int*)malloc(sizeof(int) * 2);
 	read(0, buf, SIZE);
-	ft_putstr("buf is:\n");
-	ft_putstr(buf);
+	printf("buf is:\n%s", buf);
 
-
-	if (!(buf[0] == 'o' || buf[0] == '/' || (buf[0] == 'A')))
-	{
-		ft_putstr("not a rush\n");
-	}
+	input_size(buf, &xy);
+	//xy[0] = 10;
+	//xy[1] = 20;
+	printf("x is %d, y is %d\n", xy[0], xy[1]);
 
 	return (0);
 }
+
+
+
+
+
+/*
+	if (!(buf[0] == 'o' || buf[0] == '/' || (buf[0] == 'A') || (buf[0] == 'A') || (buf[0] == 'A') || (buf[0] == 'A') || (buf[0] == 'A') ))
+	{
+		return (0);
+	}
+*/
+
